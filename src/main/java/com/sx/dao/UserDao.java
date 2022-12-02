0f8bed2a755f88da0bee132b0eeea835e69a643f
nsx <1743091664@qq.com>
2022-12-02 11:20:24 +0800

    完成登录验证

--- src/main/java/com/sx/dao/UserDao.java
+++ src/main/java/com/sx/dao/UserDao.java
@@ -1,34 +1,36 @@
 package com.sx.dao;
 
 import com.sx.bean.User;
+import org.springframework.beans.BeanUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
+import javax.rmi.CORBA.Util;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao extends BaseDao {
   /**
-   * 获取多个对象
+   * 获取用户对象
    */
   public User login(User user) {
     List<Object> args = new ArrayList<>();
     String username = user.getUsername();
     String password = user.getPassword();
 
-    // if (username == null || password == null) return;
+    if (username == null || password == null) return null;
+
     args.add(username);
     args.add(password);
     String sql = "SELECT username,password FROM users WHERE username = ? AND password=?";
-    System.out.println("query" + tpl.query(sql, new BeanPropertyRowMapper<>(User.class), args.toArray()));
-    // User use1 = tpl.query(sql, new BeanPropertyRowMapper<>(User.class), args.toArray());
-    System.out.println();
+    List<User> queryUsers = tpl.query(sql, new BeanPropertyRowMapper<>(User.class), args.toArray());
 
+    for (User queryUser : queryUsers) {
+      if(queryUser.getUsername().equals(username) && queryUser.getPassword().equals(password)){
+        return queryUser;
+      }
+    }
 
-    // System.out.println(username + " - " + password);
-    // String sql = "SELECT username,password FROM users";
-    // return tpl.query(sql, new BeanPropertyRowMapper<>(User.class));
-    // return (User) tpl.query(sql, new BeanPropertyRowMapper<>(User.class), args.toArray());
-    return user;
+    return null;
   }
 
   public List<User> list() {
